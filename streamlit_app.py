711ba8cdd8b6cff9a70a9676821874e9d609437b
bablulal <bablu.lal@gmail.com>
2024-04-13 20:37:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,69 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import cv2
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.models import load_model
+from mtcnn import MTCNN
 
-"""
-# Welcome to Streamlit!
+# Load the trained model
+model = load_model('model.h5')  # Replace with your model path
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# List of class names
+classes = ['Fair_Light', 'Medium_Tan', 'Dark_Deep']
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Mapping dictionary for descriptive skin tone labels
+descriptive_labels = {
+    'Fair_Light': 'Fair / Light',
+    'Medium_Tan': 'Medium / Tan',
+    'Dark_Deep': 'Dark / Deep'
+}
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Load the MTCNN face detection model
+mtcnn = MTCNN()
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Set app title
+st.title('ToneSense: Discovering Diversity in Skin Tones through AI')
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Upload image through file uploader
+uploaded_file = st.file_uploader('Upload an image ', type=['jpg', 'jpeg', 'png'])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# Display uploaded image
+if uploaded_file is not None:
+    image = np.array(bytearray(uploaded_file.read()), dtype=np.uint8)
+    image = cv2.imdecode(image, 1)
+    
+       
+    # Predict button
+    if st.button('Predict Skin Tone'):
+        # Detect faces
+        try:
+            faces = mtcnn.detect_faces(image)
+            if len(faces) > 0:
+                largest_face = max(faces, key=lambda f: f['box'][2] * f['box'][3])
+                x, y, w, h = largest_face['box']
+                detected_face = image[y:y+h, x:x+w]
+                
+                # Resize the detected face to the desired input shape
+                detected_face = cv2.resize(detected_face, (120, 90))
+                
+                # Preprocess the detected face for classification
+                detected_face = tf.keras.applications.mobilenet_v2.preprocess_input(detected_face[np.newaxis, ...])
+                
+                # Predict the class of the face
+                predictions = model.predict(detected_face)
+                predicted_class_idx = np.argmax(predictions)
+                predicted_class = classes[predicted_class_idx]
+                
+                # Get the descriptive label from the mapping dictionary
+                descriptive_label = descriptive_labels[predicted_class]
+                
+                # Display the prediction with a larger font and a message
+                st.write('')
+                st.write('')
+                st.write('')
+                st.write('**Predicted Skin Tone:**')
+                st.write(f'# {descriptive_label}')
+            else:
+                st.write('No face detected in the uploaded image.')
+        except Exception as e:
+            st.write(f'Error detecting faces: {e}')
